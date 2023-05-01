ec1b261e0bfb3f34b077c1ae1da0f8d2f0c2fdd7
Venkatesh Bandaru <bandaruvenkatesh0827@gmail.com>
2023-05-01 21:02:34 +0530

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from .forms import LoginForm, RegistrationForm
+from .models import User
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for('main.index'))
+
+    return render_template('login.html', form=form)
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('main.index'))
+
+    return render_template('register.html', form=form)
